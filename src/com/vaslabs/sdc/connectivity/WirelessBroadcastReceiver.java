cf59bd540811d3596b43511821e124ba06ca8b9a
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-01-23 10:31:20 +0000

    Manage onReceive event for wifip2p manager

---
+++ src/com/vaslabs/sdc/connectivity/WirelessBroadcastReceiver.java
@@ -0,0 +1,30 @@
+package com.vaslabs.sdc.connectivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+
+public class WirelessBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiP2pManager wifiP2PManager;
+    private Channel mChannel;
+    private Context context;
+    
+    public WirelessBroadcastReceiver(WifiP2pManager wifiManager, Channel channel, Context context) {
+        this.wifiP2PManager = wifiManager;
+        this.mChannel = channel;
+        this.context = context;
+    }
+    
+    @Override
+    public void onReceive( Context context, Intent intent ) {
+        String action = intent.getAction();
+        ConnectivityActionResponder actionResponder = ActionResponderFactory.getResponder(action);
+        
+        actionResponder.manageAction(context, intent);
+
+    }
+
+}
