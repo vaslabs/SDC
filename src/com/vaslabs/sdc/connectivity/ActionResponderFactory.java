1de411c749b3b82c0d1bb1df6a3b361d3ad0a366
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-01-23 11:48:56 +0000

    Manage generic views which represent the state and messages of the responders

--- src/com/vaslabs/sdc/connectivity/ActionResponderFactory.java
+++ src/com/vaslabs/sdc/connectivity/ActionResponderFactory.java
@@ -1,12 +1,14 @@
 package com.vaslabs.sdc.connectivity;
 
+import android.content.Context;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.view.View;
 
 public class ActionResponderFactory {
 
-    public static ConnectivityActionResponder getResponder( String action ) {
+    public static ConnectivityActionResponder<? extends View> getResponder( String action, Context c ) {
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals( action )) {
-            return new StateChangedConnectivityActionResponder();
+            return new StateChangedConnectivityActionResponder(c);
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
             return new PeersChangedConnectivityActionResponder();
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
--- src/com/vaslabs/sdc/connectivity/ConnectivityActionResponder.java
+++ src/com/vaslabs/sdc/connectivity/ConnectivityActionResponder.java
@@ -2,9 +2,25 @@ package com.vaslabs.sdc.connectivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
 
-public abstract class ConnectivityActionResponder {
-
+public abstract class ConnectivityActionResponder<V extends View> {
+    
+    private boolean knownState = false;
+    
     public abstract void manageAction( Context context, Intent intent );
 
+    public abstract V getView();
+    
+    public boolean stateIsKnown() {
+        return knownState;
+    }
+    
+    public void setStateKnown() {
+        knownState = true;
+    }
+    
+    public void setStateUnknown() {
+        knownState = false;
+    }
 }
