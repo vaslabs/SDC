c5c8eca447fc53b64387ebfd7f5348513a403df7
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-02-01 14:24:52 +0000

    Serialise information from string into a SkyDiver object

--- src/com/vaslabs/sdc/utils/SkyDiver.java
+++ src/com/vaslabs/sdc/utils/SkyDiver.java
@@ -1,15 +1,76 @@
 package com.vaslabs.sdc.utils;
 
+import android.util.Log;
+
 import com.vaslabs.sdc.UserInformation;
+import com.vaslabs.sdc.UserPreferences;
+import com.vaslabs.sdc.sensors.LatitudeSensorValue;
+import com.vaslabs.sdc.sensors.LongitudeSensorValue;
+import com.vaslabs.sdc.sensors.MetersSensorValue;
 
 public class SkyDiver implements PositionalUpdate {
     private UserInformation userInformation;
     private Position lastKnownPosition;
-
+    
     public SkyDiver( UserInformation ui ) {
         userInformation = UserInformation.getUserInformationClone( ui );
     }
 
+    public static SkyDiver serialiseSkyDiverFromString(String skydiverDeserialisedString) {
+        String[] mainFields = skydiverDeserialisedString.split( ":" );
+        String name, massValue, seaLevelValue, latValue, lngValue, altValue;
+        try {
+            name = mainFields[0];
+            String valuesField = mainFields[1];
+            String[] values = valuesField.split( "|" );
+            
+            massValue = values[0];
+            seaLevelValue = values[1];
+            latValue = values[2];
+            lngValue = values[3];
+            altValue = values[4];
+            
+        }
+        catch (ArrayIndexOutOfBoundsException outOfBoundsException) {
+            Log.d( "InvalidSkyDiverString", skydiverDeserialisedString );
+            return null;
+        }
+        float alt, seaLevel, mass;
+        double lng, lat;
+        try {
+            alt = Float.parseFloat( massValue );
+            lat = Double.parseDouble( latValue );
+            lng = Double.parseDouble( lngValue );
+            mass = Float.parseFloat( massValue );
+            seaLevel = Float.parseFloat( seaLevelValue );
+        }
+        catch (NumberFormatException nfe) {
+            Log.d( "InvalidSkyDiverStringValues", skydiverDeserialisedString );
+            return null;
+        }
+        
+        UserPreferences up = new UserPreferences();
+        up.mass = mass;
+        up.name = name;
+        up.seaLevel = seaLevel;
+        
+        UserInformation ui = 
+                UserInformation.getUserInformationFromUserPreferences( up );
+        
+        SkyDiver sd = new SkyDiver(ui);
+        
+        MetersSensorValue altitude = new MetersSensorValue();
+        altitude.setRawValue( alt );
+        LatitudeSensorValue latitude = new LatitudeSensorValue();
+        latitude.setRawValue( lat );
+        LongitudeSensorValue longitude = new LongitudeSensorValue();
+        longitude.setRawValue( lng );
+        Position position = new Position(longitude, latitude, altitude);
+        sd.updatePositionInformation( position );
+        
+        return sd;
+    }
+    
     @Override
     public synchronized void updatePositionInformation( Position newPosition ) {
 
