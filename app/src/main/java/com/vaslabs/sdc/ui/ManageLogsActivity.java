45f5ab9617120cdfbdf0a18a1a32fdfbe9bf7bc4
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-03-28 22:44:25 +0000

    Can submit logs - app is ready to fly and collect data for proof of concept

--- app/src/main/java/com/vaslabs/sdc/ui/ManageLogsActivity.java
+++ app/src/main/java/com/vaslabs/sdc/ui/ManageLogsActivity.java
@@ -6,18 +6,29 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import com.vaslabs.pwa.CommunicationManager;
+import com.vaslabs.pwa.Response;
 import com.vaslabs.sdc.connectivity.SkyDivingEnvironment;
+import com.vaslabs.sdc.logs.SDCLogManager;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 
 import static android.view.View.OnClickListener;
 
@@ -71,20 +82,31 @@ public class ManageLogsActivity extends Activity {
     }
 
     static public class LoginDialogFragment extends DialogFragment {
+        private EditText usernameEditText;
+        private EditText passwordEditText;
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
             // Get the layout inflater
             LayoutInflater inflater = getActivity().getLayoutInflater();
-
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
-            builder.setView(inflater.inflate(R.layout.dialog_layout, null))
+            View inflate = inflater.inflate(R.layout.dialog_layout, null);
+            usernameEditText = (EditText) inflate.findViewById(R.id.usernameEditText);
+            passwordEditText = (EditText) inflate.findViewById(R.id.passwordEditText);
+            builder.setView(inflate)
                     // Add action buttons
                     .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int id) {
-                            // sign in the user ...
+
+                            (new SubmitLogs(getActivity()))
+                                       .execute(usernameEditText.getText().toString(),
+                                               passwordEditText.getText().toString()
+                                       );
+                            Toast.makeText(getActivity(), "Submitting logs...", Toast.LENGTH_SHORT).show();
+                            LoginDialogFragment.this.getDialog().dismiss();
                         }
                     })
                     .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -92,9 +114,59 @@ public class ManageLogsActivity extends Activity {
                             LoginDialogFragment.this.getDialog().cancel();
                         }
                     });
+
             return builder.create();
         }
     }
 
+}
+
+class SubmitLogs extends AsyncTask<String, Void, Integer> {
+
+    private Context context;
+    protected SubmitLogs(Context context) {
+        this.context = context;
+    }
+    @Override
+    protected Integer doInBackground(String... params) {
+        if (params.length != 2) {
+            return HttpStatus.SC_UNAUTHORIZED;
+        }
+        CommunicationManager cm = CommunicationManager.getInstance();
+        cm.setRemoteHost( context.getString(R.string.remote_host));
+        SDCLogManager lm = SDCLogManager.getInstance(context);
+        try {
+            String username = params[0];
+            String password = params[1];
+            lm.submitLogs(username, password);
+        }
+        catch (Exception e) {
+            Log.e("Submitting logs", e.toString());
+            return HttpStatus.SC_UNAUTHORIZED;
+        }
+        String message = null;
+        try {
+            Response response = lm.getLastResponse();
+            Object responseBody = response.getBody();
+            JSONObject json = (JSONObject) responseBody;
+            message = json.getString("message");
+            if ("OK".equals(message))
+                return HttpStatus.SC_OK;
+        } catch (Exception e) {
+            Log.e("Submitting logs", e.toString());
+            return 500;
+        }
+        Log.e("SDC remote server", message);
+        return HttpStatus.SC_OK;
+    }
+
+    @Override
+    protected void onPostExecute(Integer status) {
+        if (status == HttpStatus.SC_OK) {
+            Toast.makeText(context, "Success: Logs have been submitted", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(context, "Error: " + status, Toast.LENGTH_LONG).show();
+        }
+    }
 }
 
