4446d5e32c963fdc77745bd6d582b27d0c2d28a9
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-07-03 20:19:01 +0100

    Filter barometer data to be as close to take off as possible

---
+++ app/src/androidTest/java/com/vaslabs/acceptance/TestIdentifyingSession.java
@@ -0,0 +1,55 @@
+package com.vaslabs.acceptance;
+
+import android.test.AndroidTestCase;
+
+import com.google.gson.Gson;
+import com.vaslabs.logbook.SkydivingSessionLogbookData;
+import com.vaslabs.sdc.entries.BarometerEntries;
+import com.vaslabs.sdc.entries.BarometerEntry;
+import com.vaslabs.sdc.logs.LogbookStats;
+import com.vaslabs.sdc.ui.R;
+
+import java.io.InputStreamReader;
+
+/**
+ * Created by vnicolao on 03/07/15.
+ */
+public class TestIdentifyingSession extends AndroidTestCase {
+
+    BarometerEntries barometerEntries;
+    SkydivingSessionLogbookData logbook;
+    public void setUp() {
+
+    }
+
+    private void whenJsonReceived() {
+        Gson gson = new Gson();
+        InputStreamReader jsonReader = new InputStreamReader(
+                this.mContext.getResources().openRawResource(R.raw.barometer_test_data));
+        barometerEntries = gson.fromJson(jsonReader, BarometerEntries.class);
+
+        barometerEntries.sort();
+    }
+
+    private void andSessionCreated() {
+        logbook = SkydivingSessionLogbookData.create(barometerEntries);
+    }
+
+    //1737013
+
+    private void validateThatTimesAreRight() {
+        BarometerEntry[] barometerEntries = LogbookStats.average(this.barometerEntries, 3);
+        long startedLoggingAt = barometerEntries[0].getTimestamp();
+        long filterStartsAt = logbook.getEntryAt(0).getTimestamp();
+        long difference = filterStartsAt - startedLoggingAt;
+        assertEquals(1733722, difference);
+    }
+
+    public void test_filtering_session_data_from_json() {
+        whenJsonReceived();
+        andSessionCreated();
+        validateThatTimesAreRight();
+    }
+
+
+}
---
+++ app/src/main/java/com/vaslabs/logbook/SkydivingSessionLogbookData.java
@@ -0,0 +1,79 @@
+package com.vaslabs.logbook;
+
+import com.vaslabs.sdc.entries.BarometerEntries;
+import com.vaslabs.sdc.entries.BarometerEntry;
+import com.vaslabs.sdc.logs.LogbookStats;
+
+/**
+ * Created by vnicolao on 03/07/15.
+ */
+public class SkydivingSessionLogbookData {
+
+    private BarometerEntry[] barometerEntries;
+
+    private SkydivingSessionLogbookData() {
+
+    }
+
+
+
+    public static SkydivingSessionLogbookData create(BarometerEntries barometerEntries) {
+        SkydivingSessionLogbookData logbook = new SkydivingSessionLogbookData();
+        BarometerEntry[] avgBarometerEntries = LogbookStats.average(barometerEntries, 3);
+        int takeOffIndex = findTakeOffIndex(avgBarometerEntries);
+
+        int landedIndex = findLandingIndex(avgBarometerEntries);
+
+        logbook.barometerEntries = new BarometerEntry[landedIndex + 1 - takeOffIndex];
+
+        for (int i = takeOffIndex; i <= landedIndex; i++) {
+            logbook.barometerEntries[i-takeOffIndex] = avgBarometerEntries[i];
+        }
+        return logbook;
+    }
+
+    private static int findLandingIndex(BarometerEntry[] barometerEntries) {
+        int currentIndex = 0;
+        return findLandingIndex(barometerEntries, barometerEntries.length - 10, false);
+    }
+
+    private static int findLandingIndex(BarometerEntry[] barometerEntries, int i, boolean canopy) {
+        if (!canopy) {
+            if (barometerEntries[i].getAltitude() > barometerEntries[barometerEntries.length - 1].getAltitude() + 30) {
+                return findLandingIndex(barometerEntries, i, true);
+            } else {
+                return findLandingIndex(barometerEntries, i-10, false);
+            }
+        } else {
+            if (barometerEntries[i].getAltitude() <= barometerEntries[i].getAltitude() + 10) {
+                return i;
+            } else {
+                return findLandingIndex(barometerEntries, i + 1, true);
+            }
+        }
+    }
+
+    private static int findTakeOffIndex(BarometerEntry[] avgBarometerEntries) {
+        return findTakeOffIndex(avgBarometerEntries, 100, false);
+    }
+
+    private static int findTakeOffIndex(BarometerEntry[] avgBarometerEntries, int i, boolean liftOf) {
+        if (!liftOf) {
+            if (avgBarometerEntries[i].getAltitude() > avgBarometerEntries[0].getAltitude() + 50) {
+                return findTakeOffIndex(avgBarometerEntries, i, true);
+            } else
+                return findTakeOffIndex(avgBarometerEntries, i + 100, false);
+        } else {
+            if (avgBarometerEntries[i].getAltitude() <= avgBarometerEntries[0].getAltitude() + 10) {
+                return i;
+            }
+            else {
+                return findTakeOffIndex(avgBarometerEntries, i-10, true);
+            }
+        }
+    }
+
+    public BarometerEntry getEntryAt(int i) {
+        return barometerEntries[i];
+    }
+}
